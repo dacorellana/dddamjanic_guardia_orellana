90887e51ecd92a5a3525ada89a93ea52ad15c7cf
dacorellana <54025897+dacorellana@users.noreply.github.com>
2019-08-28 14:01:40 -0400

    subidos los proyectos
    :)

--- portafolio.js
+++ portafolio.js
@@ -1,43 +1,87 @@
 var trabajos = [{
-    figure: "media/trabajo-01-desfile.jpg",
+    figure: "media/TACO 3.jpeg",
     alt: "Descripción de la imagen",
-    title: "Título de primer trabajo",
-    about: "Texto simulado de descripción de primer trabajo"
+    title: "TACO",
+    about: "Banca de terciado 3mm y amarracables.",
+    url: "https://www.instagram.com/p/B1nK5dsA3fV/"
 }, {
-    figure: "media/trabajo-02-app.jpg",
+    figure: "media/RUM 2.JPG",
     alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo"
+    title: "RUM",
+    about: "Proyecto con material Timberecco.",
+    url: "https://www.instagram.com/p/B1nI9pGAWcW/"
 }, {
-    figure: "media/trabajo-03-homy.jpg",
+    figure: "media/UNSTOP 1.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
-    about: "Texto simulado de descripción de tercer trabajo"
+    title: "UNSTOP",
+    about: "Tens inalámbrico y piezoeléctrico para deportistas imparables.",
+    url: "https://www.instagram.com/p/B1nIoSLgBjp/"
 }, {
-    figure: "media/trabajo-04-editorial.jpg",
+    figure: "media/VIRUTEX2.JPG",
     alt: "Descripción de la imagen",
-    title: "Título de cuarto trabajo",
-    about: "Texto simulado de descripción de cuarto trabajo"
+    title: "Parches VIRUTEX",
+    about: "Parches de prelavado rápido, para que las manchas no detengan tu rutina. ",
+    url: "https://www.instagram.com/p/B1nJFBoA8aF/"
+  }, {
+    figure: "media/AGUAU.JPG",
+    alt: "Descripción de la imagen",
+    title: "AGUAU",
+    about: "Recipiente de agua para perros diseñado para la fundación Miradanimal. ",
+    url: "https://www.instagram.com/p/B1nKyIhAHjI/"
 }, {
-    figure: "media/trabajo-05-artesania.jpg",
+    figure: "media/canvas.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de quinto trabajo",
-    about: "Texto simulado de descripción de quinto trabajo"
+    title: "CANVAS",
+    about: "Polera que captura las caídas que convellan la práctica del BMX.",
+    url: "https://www.instagram.com/p/B1nIhT-Ajqb/"
 }, {
-    figure: "media/trabajo-06-textil.jpg",
+    figure: "media/Fotomontaje Surrealista.png",
     alt: "Descripción de la imagen",
-    title: "Título de sexto trabajo",
-    about: "Texto simulado de descripción de sexto trabajo"
+    title: "Fotomontaje Surrealista",
+    about: "Trascender lo real a partir del impulso psíquico del estudiante, de lo imaginado e irracional.",
+    url: "https://www.instagram.com/p/B1nJ86ZA5ML/"
+  }, {
+    figure: "media/eternocautiva.png",
+    alt: "Descripción de la imagen",
+    title: "Eterno Cautiva",
+    about: "Creación de logotipo e isótopo para empresa PyME.",
+    url: "https://www.instagram.com/p/B1nKz45gI10/"
 }, {
-    figure: "media/trabajo-07-materiales.jpg",
+    figure: "media/soportecelular.png",
     alt: "Descripción de la imagen",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
+    title: "Soporte Celular",
+    about: "Fusion 360 en la creación del soporte e impresión de prototipo en Impresoras 3D.",
+    url: "https://www.instagram.com/p/B1nKklXgxEA/"
+}, {
+    figure: "media/dibujotecnico.png",
+    alt: "Descripción de la imagen",
+    title: "Dibujo Técnico ISO-E",
+    about: "Líneas normalizadas, continuas y segmento, una isometría del mismo objeto, sin corte. Formato A-3.",
+    url: "https://www.instagram.com/p/B1nKwHNgztk"
+}, {
+    figure: "media/IECAM.png",
+    alt: "Descripción de la imagen",
+    title: "Logotipo Empresa PyME Certificadora de Ascensores",
+    about: "Imagen Corporativa para dicha empresa.",
+    url: "https://www.instagram.com/p/B1nK66IA5Z_/"
+    }, {
+    figure: "media/huertosur.PNG",
+    alt: "Descripción de la imagen",
+    title: "Huerto Sur Accesible",
+    about: "Rediseño del café Huerto Sur para hacerlo  un espacio inclusivo. ",
+    url: "https://www.instagram.com/p/B1nLioogbJu/"
+      }, {
+    figure: "media/CARBRO.JPG",
+    alt: "Descripción de la imagen",
+    title: "CARBRO ",
+    about: "Producto para entrenar la memoria muscular del equilibrio utilizado en deportes como el surf y el surfskate.",
+    url: "https://www.instagram.com/p/B1nL9UVgoBr/"
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
     var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+    var cuatro = '<p><a href= "' + trabajos[i].url + ' ">Ver más...</a></p>'
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro + '</div>')
 });
